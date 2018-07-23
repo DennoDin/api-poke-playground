4161bae39e88ad69cedfdfea8d66ed5e5e276cb5
Code Chrysalis <codechrysalis@Codes-Mac-mini.local>
2018-07-23 18:46:34 +0900

    Created findNames function in pokemonager.js
    Co-authored-by: Dustin Tran <dustin.andy.tran@gmail.com>
    Co-authored-by: Hiroyuki Momoi <hirocorp01@gmail.com>

--- pokemonager.js
+++ pokemonager.js
@@ -3,6 +3,17 @@
     // This should return an array of all the names of n Pokemon from the Pokemon API.
     findNames(n) {
       // Your code here.
+      let result = [];
+      let promise;
+      promise = axios
+        .get(`https://pokeapi.co/api/v2/pokemon/?limit=${n}&offset=0"`)
+        .then((pokemon) => {
+          result = pokemon.data.results.map((item) => {
+            return item.name;
+          });
+          return result;
+        });
+      return promise;
     }
 
     // This should return an array of all the Pokemon that are under a particular weight.
